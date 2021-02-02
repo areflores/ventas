b931060b3cd452b4d2e45f830c03cbe4f44f4cef
Areli Flores <floresareli1998@gmail.com>
2021-02-01 23:30:08 -0600

    agregando frase motivadora

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola mundo");
+            MessageBox.Show("Piensa, cree, sueña y atravete");
         }
     }
 }
